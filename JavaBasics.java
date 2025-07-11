e270a8402bf84faf4f793061779e98add3ef61c7
Shankar-Sala <Shankarsala9995@gmail.com>
2025-07-11 13:11:42 +0530

    do-while loop

--- JavaBasics.java
+++ JavaBasics.java
@@ -434,21 +434,35 @@
 
 
 // Reverse the given number 
+// import java.util.*;
+
+// public class JavaBasics{
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+     
+//      System.out.print("Enter your number: ");
+//      int n = sc.nextInt();
+//      int rev = 0;
+
+//      while(n > 0){
+//       int lastDigit = n % 10;
+//       rev = ((rev * 10) + lastDigit);
+//       n /= 10;
+//      }
+//      System.out.println("Reverse of given number: "+ rev);
+//   }
+// }
+
+//do-while loop
 import java.util.*;
 
 public class JavaBasics{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
-     
-     System.out.print("Enter your number: ");
-     int n = sc.nextInt();
-     int rev = 0;
-
-     while(n > 0){
-      int lastDigit = n % 10;
-      rev = ((rev * 10) + lastDigit);
-      n /= 10;
-     }
-     System.out.println("Reverse of given number: "+ rev);
+    int counter = 1;
+    do{
+      System.out.println("Hello world");
+      counter++;
+    }while(counter <= 10);
   }
 }
